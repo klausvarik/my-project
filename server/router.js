e451df30e7914434ba2bdbcee8220f0828a76d34
Klaus Varik <klaus.varik@vikk.ee>
2021-11-25 10:35:52 +0200

    commit

---
+++ server/router.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const { Todos } = require("./dbConnection");
+
+router.get("/calc", function (req, res) {
+  const a = 4;
+  const b = 4;
+  const result = a + b;
+  res.send({ result });
+});
+
+// request === req ja response === res
+router.get("/get-todos", async function (request, response) {
+  const result = await Todos.find();
+  console.log(result);
+  response.send(result);
+});
+
+router.get("/delete-todo/:id", async function (request, response) {
+  await Todos.deleteOne({ _id: request.params.id });
+  console.log("Delete todo..");
+  response.send({});
+});
+
+router.get("/get-todo/:id", async function (request, response) {
+  const result = await Todos.findOne({ _id: request.params.id });
+  console.log(result);
+  response.send(result);
+});
+
+router.post("/update-todo/:id", async function (request, response){
+  await Todos.updateOne(
+    { _id: request.params.id },
+    { $set: { status: request.body.status } }
+  );
+  console.log("updating todo..");
+  response.send({});
+});
+
+router.post("/add-todo", async function (request, response) {
+  if (request.body.title) {
+    await Todos.create(request.body);
+    console.log("Lisa todo");
+  }
+  response.send("done");
+});
+
+module.exports = router;
---
+++ src/components/TodoList.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <h2>{{ title }}</h2>
+
+        <ul class="list-group mb-3">
+          <li
+            v-for="todo in todosFromServer"
+            :key="todo"
+            class="list-group-item"
+            @click="getTodo(todo._id)"
+          >
+            {{ todo.title }} {{ todo.status }}
+            <button @click="deleteTodo(todo._id)">Delete</button>
+            <button @click="completeTodo(todo._id)">Done</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <input
+          v-model="newTodo"
+          type="text"
+          name="newTodo"
+          class="form-control"
+        />
+      </div>
+      <div class="col">
+        <button @click="addTodo" type="submit" class="btn btn-primary w-100">
+          Add new todo
+        </button>
+      </div>
+    </div>
+    {{ singleTodo }}
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import axios from "axios";
+
+export default {
+  name: "TodoList",
+  props: {
+    title: String,
+  },
+  setup() {
+    const todos = ref(["Read a book", "Go for a walk", "Eat food"]);
+    const newTodo = ref("");
+    const todosFromServer = ref([]);
+    const singleTodo = ref({});
+
+    async function deleteTodo(id){
+      await axios.get("/api/delete-todo/" + id);
+      await getTodos();
+    }
+
+    async function completeTodo(id){
+      await axios.post("/api/update-todo/" + id, { status : 'COMPLETE' });
+      await getTodos();
+    }
+    async function getTodos() {
+      const result = await axios.get("/api/get-todos");
+      todosFromServer.value = result.data;
+      console.log(result.data);
+    }
+
+    async function getTodo(id) {
+      const result = await axios.get("/api/get-todo/" + id);
+      singleTodo.value = result.data;
+      console.log(result.data);
+    }
+
+    async function addTodo() {
+      await axios.post("/api/add-todo", {
+        title: newTodo.value,
+        status: "ACTIVE",
+      });
+      newTodo.value = "";
+      await getTodos();
+    }
+
+    getTodos();
+
+    function addNewTodo() {
+      todos.value.push(newTodo.value);
+      newTodo.value = "";
+    }
+
+    return {
+      todos,
+      newTodo,
+      addNewTodo,
+      todosFromServer,
+      addTodo,
+      singleTodo,
+      getTodo,
+      deleteTodo,
+      completeTodo,
+    };
+  },
+  methods: {
+    changeState(input) {
+      if (input === "ACTIVE") {
+        return this.todoComplete;
+      } else {
+        return this.todoActivate;
+      }
+    },
+  },
+};
+</script>
