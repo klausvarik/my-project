955532f0ed293745e51d2cc9e0babbe10bce482f
Klaus Varik <klaus.varik@vikk.ee>
2021-11-25 10:44:30 +0200

    database

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Connecting to database
 mongoose.connect(
-  "mongodb+srv://test:test@cluster0.l3qaq.mongodb.net/todoapp?retryWrites=true&w=majority",
+  "mongodb+srv://klaus:NTt6_6fqUwU4kBz@cluster0.mwojn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
 const Schema = mongoose.Schema;
